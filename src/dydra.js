0770731a526f52f92f2db0384eddf1bc706832d1
Arto Bendiken <arto@bendiken.net>
2014-04-18 02:35:25 +0200

    Defined the Dydra.Session constructor.

--- src/dydra.js
+++ src/dydra.js
@@ -5,3 +5,22 @@
  *
  * @see http://docs.dydra.com/sdk/javascript
  */
+
+var Dydra = (function($) {
+  var Session = function(credentials) {
+    this.query = function() {
+      // TODO
+    };
+  };
+
+  return {
+    Session: Session
+  };
+})(jQuery);
+
+if (typeof exports !== 'undefined') {
+  module.exports = Dydra;
+}
+else {
+  window.Dydra = Dydra;
+}
