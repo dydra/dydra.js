047498c625e5a0a0af6ca7c2d9ea05af76afd874
Arto Bendiken <arto@bendiken.net>
2014-04-18 12:31:59 +0200

    Implemented SPARQL.Query#ask(), #construct(), #describe(), #select().

--- src/sparql.js
+++ src/sparql.js
@@ -8,7 +8,7 @@
 
 var SPARQL = (function($) {
   var Client = function(endpointURL) {
-    this.query = function(queryText, settings) {
+    this.query = function(queryText, options) {
       $.ajax(endpointURL, {
         type: 'POST',
         contentType: 'application/sparql-query',
@@ -18,20 +18,45 @@ var SPARQL = (function($) {
         beforeSend: function(xhr) {
           xhr.setRequestHeader('Accept', 'application/sparql-results+json');
         },
-        username: settings.username,
-        password: settings.password,
-        success: settings.success,
-        error: settings.failure
+        username: options.username,
+        password: options.password,
+        success: options.success,
+        error: options.failure
       });
     };
 
-    this.update = function(queryText, settings) {
+    this.update = function(queryText, options) {
       // TODO
     };
   };
 
-  var Query = function(settings) {
-    this.queryForm = settings.form || "SELECT";
+  var Query = function(options) {
+    if (options === undefined) options = {};
+    this.form = options.form || "SELECT";
+    this.variables = [];
+
+    this.ask = function(options) {
+      this.form = "ASK";
+      return this;
+    };
+
+    this.select = function(variables, options) {
+      this.form = "SELECT";
+      this.variables = variables;
+      return this;
+    };
+
+    this.describe = function(variables, options) {
+      this.form = "DESCRIBE";
+      this.variables = variables;
+      return this;
+    };
+
+    this.construct = function(patterns, options) {
+      this.form = "CONSTRUCT";
+      this.patterns = patterns;
+      return this;
+    };
   };
 
   return {
--- test/sparql.query.js
+++ test/sparql.query.js
@@ -1,3 +1,19 @@
-test("todo", function() {
-  expect(0);
+test("default query form", function() {
+  equal((new SPARQL.Query).form, "SELECT", "expected default query form to be SELECT");
+});
+
+test("ASK construction", function() {
+  equal((new SPARQL.Query).ask().form, "ASK", "expected query form to be ASK");
+});
+
+test("CONSTRUCT construction", function() {
+  equal((new SPARQL.Query).construct().form, "CONSTRUCT", "expected query form to be CONSTRUCT");
+});
+
+test("DESCRIBE construction", function() {
+  equal((new SPARQL.Query).describe().form, "DESCRIBE", "expected query form to be DESCRIBE");
+});
+
+test("SELECT construction", function() {
+  equal((new SPARQL.Query).select().form, "SELECT", "expected query form to be SELECT");
 });
