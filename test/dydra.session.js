dbbb12cafd0201219f0de3da1ddb9e0a3e83514a
Arto Bendiken <arto@bendiken.net>
2014-04-18 22:21:05 +0200

    Made the README example code into a test case.

--- test/dydra.session.js
+++ test/dydra.session.js
@@ -1,3 +1,21 @@
-test("todo", function() {
-  expect(0);
+asyncTest("README example", function() {
+  var mySession = new Dydra.Session(/*{token: "MY_SECRET_TOKEN"}*/);
+
+  var myRepository = mySession.open("jhacker/foaf");
+
+  myRepository.query("SELECT * WHERE {?s ?p ?o} LIMIT 10", {
+    context: this,
+    success: function(data) {
+      console.log("SPARQL query succeeded and returned:");
+      console.log(data);
+      ok(true);
+      start();
+    },
+    failure: function(xhr) {
+      console.log("SPARQL query failed.");
+      console.log(xhr);
+      ok(false);
+      start();
+    }
+  });
 });
